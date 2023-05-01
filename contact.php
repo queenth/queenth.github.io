f34726d600db13971086064a4cc10b1c3e420345
queenth <131933109+queenth@users.noreply.github.com>
2023-05-01 21:40:03 +0100

    my first commit

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php
+ 
+
+ isset($_POST['submit']) 
+
+	$name = $_POST['name'];
+	$gender = $_POST['gender'];
+	$area = $_POST['area_choice'];
+	$how = $_POST['hear'];
+	$email = $_POST['email']
+	$subject = $_POST['subject']
+	$message = $_POST['message']
+
+
+	echo "Name: $name, Gender: $gender, Area(s) of Interest: $area_choice, Subject: $subject "
+	echo  Message: $message."
+	 
+	echo "Hello $name, We have received your Response and will be in touch soon."
+}
+
+?>
+
+
